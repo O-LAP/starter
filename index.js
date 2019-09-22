d4ae161e6c1d15451aa1b3316b6280f189d00b2c
amitlzkpa <amit.lzkpa@gmail.com>
2019-09-22 15:28:00 -0400

    setup 404

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('static'));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('*', function(req, res){
+  res.status(404).send('Not found');
 });
 
 app.listen(PORT, () => {
